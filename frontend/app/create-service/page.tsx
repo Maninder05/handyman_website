fd1eb952dab25eddebea7ce03e82d7c32c4598fb
Maninder05 <165261256+Maninder05@users.noreply.github.com>
2025-09-24 14:27:48 -0600

    commit

--- frontend/app/create-service/page.tsx
+++ frontend/app/create-service/page.tsx
@@ -46,7 +46,7 @@ export default function CreateService() {
     if (Object.keys(newErrors).length > 0) {
       setErrors(newErrors);
     } else {
-      setPopup("Service Submitted Successfully ✅");
+      setPopup("Service Submitted Successfully");
     }
   };
 
