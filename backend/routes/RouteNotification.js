1ccd3a9680e4d3af1ff52c6cbf099a3110e3692b
Maninder05 <165261256+Maninder05@users.noreply.github.com>
2025-10-22 06:26:00 -0600

    commit

---
+++ backend/routes/RouteNotification.js
@@ -0,0 +1,11 @@
+// routes/notificationRoutes.js
+const express = require('express');
+const router = express.Router();
+const { getNotifications, markAsRead, markAllRead } = require('../controllers/notificationController');
+const auth = require('../middleware/auth'); // put your auth middleware
+
+router.get('/', auth, getNotifications);
+router.post('/mark-read/:id', auth, markAsRead);
+router.post('/mark-all-read', auth, markAllRead);
+
+module.exports = router;
